26e77dc11dc88c6f2377973143105c6a1f241be8
Moufid Zekiouk <moufid.zekiouk@studenti.unina.it>
2026-03-06 20:36:03 +0000

    aggiunto header

--- un_primo_esempio_di_server_multithread/Header.h
+++ un_primo_esempio_di_server_multithread/Header.h
@@ -4,8 +4,19 @@
 #define CLIENT 3
 
 /* TBD: Definire una struct "msg_richiesta" per i messaggi dal client al server */
+struct msg_richiesta{
+       long pid;
+	int v1;
+	int v2;
 
+
+};
 /* TBD: Definire una struct "msg_risposta" per i messaggi dal server al client */
+struct msg_risposta{
+	long pid;	
+	int v3;
+};
+
 
 void client(int id_c, int id_s);
 void server(int id_c, int id_s);
